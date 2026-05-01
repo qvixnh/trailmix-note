4f4c9efa4c0bc0f43c86f88d6946a6f964edb7fe
nqvinhpersonal <nguyenvinh22012002@gmail.com>
2026-05-01 21:40:01 +0700

    Lightning Web Components for Visualforce Developers Work with salesforce Data

--- force-app/main/default/lwc/accountFinder/accountFinder.js
+++ force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -1,4 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
 
 export default class AccountFinder extends LightningElement {
     annualRevenue = null;
@@ -10,4 +11,7 @@ export default class AccountFinder extends LightningElement {
     reset() {
         this.annualRevenue = null;
     }
+
+    @wire(queryAccountsByRevenue, { annualRevenue: '$annualRevenue' })
+    accounts;
 }
